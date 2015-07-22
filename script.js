36ff4d99cdddeaf8f427b88b7811ce404daadef5
kevin-verschaeve <kevin.verschaeve@live.fr>
2015-07-22 12:23:47 +0200

    mise a jour de la map en fonction de la saisie de l'utilisateur, sépatation de l'envoi sur un choix de l'autocomplete ou par l'envoi du user

--- script.js
+++ script.js
@@ -30,37 +30,64 @@ var controller = app.controller('myCtrl', function($scope, myService) {
 	$scope.viewMap = function() {
 		console.log(myService.getMap());
 	};
+
+	$scope.isEmpty = function(value) {
+		return (typeof value == "undefined" || value == "" || value == 0 || value == false);
+	};
+
+	$scope.getAndApplyMap = function() {
+		if ($scope.isEmpty($scope.chosenPlace)) {
+			return;
+		}
+
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({address: $scope.chosenPlace}, function(results, status) {
+
+			if (results.length < 1) {
+				return;
+			}
+
+			latitude = results[0].geometry.location.lat();
+			longitude = results[0].geometry.location.lng();
+
+			$scope.applyMap({latitude: latitude, longitude: longitude});
+		});
+	};
+
+	$scope.applyMap = function(center) {
+		$scope.$apply(function() {
+			$scope.map.center = center;
+
+			myService.setMap($scope.map);
+		});
+	};
 });
 
 // autocomplete gmap
 app.directive('googleplace', function(myService) {
-    return {
-	    require: 'ngModel',
-	    scope: false, // en mettant scope a false, on dit de ne pas créer de nouveau scope pour la directive, c'est donc le scope du parent qui sera pris, ce qui nous interesse ici
-	    link: function(scope, element, attrs, model) {
-	        var options = {
-	            types: ['address'],
-	        };
+	return {
+		require: 'ngModel',
+		scope: false, // en mettant scope a false, on dit de ne pas créer de nouveau scope pour la directive, c'est donc le scope du parent qui sera pris, ce qui nous interesse ici
+		link: function(scope, element, attrs, model) {
+			var options = {
+				types: ['address'],
+			};
 
-	        scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+			scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
-	        google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-	            var geoComponents = scope.gPlace.getPlace();
-	            var latitude = geoComponents.geometry.location.lat();
-	            var longitude = geoComponents.geometry.location.lng();
+			google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+				var geoComponents = scope.gPlace.getPlace();
 
-	            scope.$apply(function() {
-	            	scope.map.center.latitude = latitude;
-	            	scope.map.center.longitude = longitude;
-
-	            	myService.setMap(scope.map);
-
-	                model.$setViewValue(element.val());
-	            });
-	        });
-	    }
+				// si lat ou lng est empty, on recupere les coordonnées par l'adresse dans l'input
+				if (scope.isEmpty(geoComponents.latitude) || scope.isEmpty(geoComponents.longitude)) {
+					scope.getAndApplyMap();
+				} else { // ici, on a cliqué sur une proposition de l'autocomplete
+					scope.applyMap({
+						latitude: geoComponents.geometry.location.lat(),
+						longitude: geoComponents.geometry.location.lng()
+					});
+				}
+			});
+		}
 	};
 });
-
-
-
